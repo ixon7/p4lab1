1f586120306e46ce1f86b53741e7a34a9a845f26
ixon7 <55660649+ixon7@users.noreply.github.com>
2020-03-07 10:55:36 +0100

    Fixed to .net core 3.1

---
+++ p4lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace p4lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            crud.Create(conn);
+            crud.Read(conn);
+        }
+    }
+}
---
+++ p4lab1/crud.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p4lab1
+{
+    public static class crud
+    {
+        public static void Create(SqlConnection conn)
+        {
+            string sql = "INSERT INTO dbo.Region(RegionId,RegionDescription) VALUES (5,'@regionName');";
+            SqlCommand command = new SqlCommand(sql, conn);
+
+            int a = command.ExecuteNonQuery();
+            if (a > 0)
+            {
+                Console.WriteLine("Wpisano");
+            }
+        }
+
+        public static void Read(SqlConnection connect)
+        {
+            SqlCommand command = new SqlCommand
+            {
+                CommandText = "SELECT * FROM dbo.Customers",
+                Connection = connect,
+            };
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CompanyName"]);
+            }
+            reader.Close();
+        }
+
+        public static void Update(SqlConnection connect)
+        {
+            connect.Open();
+            string sql = "UPDATE Region SET RegionDescription = @regionName WHERE RegionId = @id";
+
+            var command = new SqlCommand(sql);
+            command.Parameters.AddWithValue("id", "");
+            command.Parameters.AddWithValue("regionName", "");
+
+            command.ExecuteNonQuery();
+
+            connect.Close();
+        }
+    }
+}
